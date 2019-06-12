73e109b3f35c28a553892a9632c766e7608be7f8
Igor Abilio <igor.abilio@gmail.com>
2019-06-12 17:30:15 +0100

    Creating users controller

---
+++ TB.Web.Api/Controllers/BaseController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using TB.Aplication.DTO;
+using TB.Aplication.Interfaces;
+using TB.Domain.Entities;
+
+namespace TB.Service.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class BaseController<Entity, DTOEntity> : Controller
+        where Entity : BaseEntity
+        where DTOEntity : BaseDTO
+    {
+        readonly protected IBaseApp<Entity, DTOEntity> app;
+
+        public BaseController(IBaseApp<Entity, DTOEntity> app)
+        {
+            this.app = app;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Get()
+        {
+            try
+            {
+                var data = app.GetAll();
+                return new OkObjectResult(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            try
+            {
+                var data = app.GetById(id);
+                return new OkObjectResult(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] DTOEntity data)
+        {
+            try
+            {
+                return new OkObjectResult(app.Add(data));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] DTOEntity data)
+        {
+            try
+            {
+                app.Update(data);
+                return new OkObjectResult(true);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Remove(Guid id)
+        {
+            try
+            {
+                app.Remove(id);
+                return new OkObjectResult(true);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
